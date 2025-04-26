6749bd41293d55361bf3835395c4bf79c10568fd
Dhanush B A <dhanushba361@gmail.com>
2025-04-26 09:53:43 +0530

    first commit

---
+++ api_credentials.h
@@ -0,0 +1,11 @@
+#ifndef API_CREDENTIALS_H
+#define API_CREDENTIALS_H
+
+#include <cstdlib>  // For std::getenv
+#include <string>
+
+const std::string CLIENT_ID = "vntEtyt8";
+const std::string CLIENT_SECRET = "XaC01v-nEcThOB0F_FDleZa77ZZLXcJmBBcbPfg1d3g";
+
+#endif // API_CREDENTIALS_H
+
